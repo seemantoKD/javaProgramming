b5f9c92f4660d39b41b37b3c478516545116195e
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2026-01-12 22:46:29 +0600

    Basic syntax, Variable and Data types

---
+++ module01/Print.java
@@ -0,0 +1,24 @@
+package com.mycompany.basicjava.module01;
+
+public class Print {
+    public static void main(String[] args){
+        // System.out.println("Hello Seemanto!");
+        // System.out.println("Lets start Coding!");
+        // int age = 21;
+        // System.out.printf("My age is %d\n",age);
+
+        // assignment 1
+        System.out.println("id: 101,");
+        System.out.println("title: iphone15,");
+        System.out.println("price: 1895 euros,");
+        System.out.println("description: perfect product with best image quality,");
+        System.out.println("category: phone.");
+    }
+}
+
+/*
+    learning:
+        1. print -> print()
+        2. print with newline -> println()
+        3. cpp formatted print -> printf()
+*/
