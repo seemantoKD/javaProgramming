beaccfe5285cbfbe6163dd716edf41b1ff994983
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2026-01-17 19:22:08 +0600

    DateClass, TimeClass, RandomNumber, NumberSystem

---
+++ module06/NumberSystemConversation.java
@@ -0,0 +1,32 @@
+package com.mycompany.basicjava.module06;
+
+import java.util.Scanner;
+
+public class NumberSystemConversation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        /*
+         * // any number system to decimal number system
+         * String binary ="1010";
+         * Integer decimal = Integer.parseInt(binary,2);
+         * System.out.println(decimal);
+        */
+
+        /*
+         * // decimal to any number system
+         * int decimal = 11;
+         * String hexa = Integer.toHexString(decimal);
+         * System.out.println(hexa);
+        */
+
+        input.close();
+    }
+}
+
+
+/*
+    learning:
+        1. deciaml to any number system
+        2. any number system to decimal
+*/
